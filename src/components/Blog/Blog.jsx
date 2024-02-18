77066480910b76fa8dba2beed58467fff7ccdbd7
Meryembarkallah21 <meryembarkallah21@gmail.com>
2024-02-18 18:41:57 +0100

    Initial commit

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -17,7 +17,7 @@ export default function Blog() {
                             <h3>Unlock Success with La Magie des Montages et Construction Métallique</h3>
                             <p>Explore our wealth of experience gained from working across multiple countries and rest assured of 100% project success with our expertise in metal fabrication and construction.</p>
                             <div className="btn_wrapper">
-                            <a className="btn" href="/" target="_blank" rel="noopener noreferrer">Read Owner's Resume {faRocketIcon}</a>
+                            <a className="btn" href="https://drive.google.com/file/d/1mxK1tv4AHzJvLKVSH6UWQ6mIIa2JgN7A/view?usp=sharing" target="_blank" rel="noopener noreferrer">Read Owner's Resume {faRocketIcon}</a>
                             </div>
                         </div>
                         <div className="blog_col">
